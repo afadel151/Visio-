4c33b49b775b0c8c17d069a57156eeabf70e3443
Fadel Akram <af8356207@gmail.com>
2024-06-19 11:44:25 +0100

    Update create.blade.php

--- planification/resources/views/weeks/final/create.blade.php
+++ planification/resources/views/weeks/final/create.blade.php
@@ -17,6 +17,18 @@
 
             </div>
 
+     <div class="h-[100px]  flex justify-around items-center  ">
+
+            <div class="flex h-[100px] space-x-3 justify-center items-center ">
+                <button class="btn glass rounded-xl " id="export_excel">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M9 8.25H7.5a2.25 2.25 0 0 0-2.25 2.25v9a2.25 2.25 0 0 0 2.25 2.25h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25H15M9 12l3 3m0 0 3-3m-3 3V2.25" />
+                    </svg>
+                </button>
+
+            </div>
 
 
         </div>
